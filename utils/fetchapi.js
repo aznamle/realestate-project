04e38a8d8950463a84dcb46077532141b99ba309
aznamle <namle1998@hotmail.com>
2022-04-07 18:14:43 -0700

    added layouts, providers, fetchapi util

---
+++ utils/fetchapi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+            'X-RapidAPI-Key': '399b595699msha2229b021447d64p1a159djsndbbf792e8015'
+          }
+    });
+
+    return data;
+}
